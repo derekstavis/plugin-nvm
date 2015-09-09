dbaea1a0f75615886566fa9ca24e166c8c294fa4
Derek Stavis <dekestavis@gmail.com>
2015-09-09 00:26:06 -0300

    don't download nvm automatically

--- nvm.fish
+++ nvm.fish
@@ -6,23 +6,15 @@
 #
 
 function init -a path --on-event init_nvm
-  if not available 'fenv'
+  if not available fenv
     omf install foreign-env
-  end
-
-  if not test -e ~/.nvm/nvm.sh
-    if available wget
-        echo "Installing nvm..."
-        wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
-    else if available curl
-        echo "Installing nvm..."
-        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
-    else
-        echo "You need to install nvm"
-    end
   end
 end
 
 function nvm -d "My package"
-  fenv source ~/.nvm/nvm.sh\; nvm $argv
+  if test -e ~/.nvm/nvm.sh
+    fenv source ~/.nvm/nvm.sh\; nvm $argv
+  else
+    echo "You need to install nvm"
+  end
 end
