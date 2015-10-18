c0d8683c31c492b959055488bf9c52102668a80b
Derek Stavis <dekestavis@gmail.com>
2015-10-18 03:00:01 -0200

    init: Fix typo in NVM_DIR fallback

--- init.fish
+++ init.fish
@@ -2,7 +2,7 @@ function init -a path --on-event init_nvm
   require foreign-env
 
   if available fenv
-    set -q NVM_DIR; or set -gx NVM_DIR $NVM_DIR
+    set -q NVM_DIR; or set -gx NVM_DIR ~/.nvm
     fenv source $NVM_DIR/nvm.sh >/dev/null ^&1
   end
 
