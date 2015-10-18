fcb251661a01795238bb542449cf0909f35318f9
Derek Stavis <dekestavis@gmail.com>
2015-10-17 21:24:55 -0300

    init: Initilialise nvm on shell start
    functions/nvm: Use NVM_DIR variable exported by init.

--- functions/nvm.fish
+++ functions/nvm.fish
@@ -6,9 +6,9 @@
 #
 
 function nvm -d "Node version manager"
-  if test -e ~/.nvm/nvm.sh
+  if test -e $NVM_DIR/nvm.sh
     available fenv;
-      and fenv source ~/.nvm/nvm.sh\; nvm $argv
+      and fenv source $NVM_DIR/nvm.sh\; nvm $argv
       or echo "You need to install foreign-env plugin"
   else
     echo "You need to install nvm"
--- init.fish
+++ init.fish
@@ -1,3 +1,9 @@
 function init -a path --on-event init_nvm
   require foreign-env
+
+  if available fenv
+    set -q NVM_DIR; or set -gx NVM_DIR $NVM_DIR
+    fenv source $NVM_DIR/nvm.sh >/dev/null ^&1
+  end
+
 end
